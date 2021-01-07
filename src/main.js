089aafe959569c53a388a5d67fe79d159d0f719e
adrienbannwarth <adrien.bann@gmail.com>
2021-01-07 23:25:59 +0100

    publish site

--- src/main.js
+++ src/main.js
@@ -2,35 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import ScrollAnimation from "./directives/scrollanimation";
-// import VueI18n from "vue-i18n";
-// import moment from "moment";
 
 Vue.directive("scrollanimation", ScrollAnimation);
-// function loadLocaleMessages() {
-//   const locales = require.context("./lang", true, /[A-Za-z0-9-_,\s]+\.json$/i);
-//   const messages = {};
-//   locales.keys().forEach(key => {
-//     const matched = key.match(/([A-Za-z0-9-_]+)\./i);
-//     if (matched && matched.length > 1) {
-//       const locale = matched[1];
-//       messages[locale] = locales(key);
-//     }
-//   });
-//   return messages;
-// }
 
-// let locale = navigator.language.substring(0, 2);
-// moment.locale(locale);
-
-// const i18n = new VueI18n({
-//   fallbackLocale: "en",
-//   locale: locale,
-//   messages: loadLocaleMessages()
-// });
 
 new Vue({
   router,
-  // i18n,
   render: h => h(App)
 }).$mount("#app");
 
